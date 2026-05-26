1a23674b6a0cf64a0f1d60c949fe7a2f4b8becb2
Samantha <smnthmk@users.noreply.github.com>
2026-05-26 23:18:32 +0800

    Revert "Merge branch 'main' of https://github.com/casjrn/126-final-project"
    This reverts commit 53cb35b5804a1edc78d04f13856c25fa692be3ae, reversing
    changes made to 607b9800e4ab3827397928a48180be91b1ad4ec7.

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:upesov/theme/upesov_theme.dart';
 import 'package:upesov/features/pages/landing_page.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-
-  // 4. Initialize Supabase
-  await Supabase.initialize(
-    url: 'https://zcmbtclypnsbsxfpfgmk.supabase.co',
-    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpjbWJ0Y2x5cG5zYnN4ZnBmZ21rIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU2NjQwMDUsImV4cCI6MjA5MTI0MDAwNX0.OXPwbU8omslvmeRX66idiiKZHGQbxPS75T-htLZOEL4',
-  );
-
+void main() {
   runApp(const MyApp());
 }
 
@@ -33,3 +24,4 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
