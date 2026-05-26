3abca9b59135ded49ba776b1743f3fd2119f7db4
Samantha <smnthmk@users.noreply.github.com>
2026-05-27 01:22:27 +0800

    restored auth with updated redirect to dash

--- lib/features/pages/dashboard.dart
+++ lib/features/pages/dashboard.dart
@@ -1,50 +1,21 @@
 import 'package:flutter/material.dart';
-//import 'package:flutter/gestures.dart';
 import 'package:upesov/theme/upesov_theme.dart';
 import 'package:upesov/features/widgets/navbar.dart';
-//import 'package:upesov/features/pages/login_page.dart';
-//import 'package:upesov/features/pages/setup_page.dart';
-import 'package:flutter/widget_previews.dart';
 
-  //preview of dashboard page layout
-  @Preview(name: 'Dashboard Layout Preview')
-  Widget previewDashboard() {
-    return DashboardPage();
-  }
-
-  class DashboardPage extends StatefulWidget{
-    const DashboardPage({super.key});
-
-    @override
-    State<DashboardPage> createState() => _DashboardPageState();
-
-  //===== FRONTEND =====
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: AppColors.background,
-      appBar: const CustomNavBar(currentPage: 'DASHBOARD'), 
-      body: Row(
-        children: [
-          
-        ],
-      )
-    );
-
-  }
-
-
-
-
-/*
 class DashboardPage extends StatelessWidget {
+  const DashboardPage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.background,
-
       appBar: const CustomNavBar(currentPage: 'DASHBOARD'), 
-      
+      body: const Center(
+        child: Text(
+          'Dashboard Placeholder',
+          style: TextStyle(color: Colors.white, fontSize: 20),
+        ),
+      ),
     );
   }
-}*/
+}
